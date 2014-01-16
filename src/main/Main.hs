c25d00dbf0c3250d6bd6ce5cd7cd5f13a6c21436
Kwang Yul Seo <kwangyul.seo@gmail.com>
2014-01-17 00:58:13 +0900

    Pass the filename to interpreter as the command line argument.
    Use optparse-applicative package to handle command line arguments.

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -3,10 +3,21 @@ module Main where
 import FP.Parser
 import FP.Interpreter
 
-main :: IO ()
-main =  do
-    source <- getContents
+import Control.Applicative
+import Data.Monoid
+import Options.Applicative
+
+data Option = Option { filename :: String }
+
+runWithOptions :: Option -> IO ()
+runWithOptions opts = do
+    source <- readFile $ filename opts
     case parseFP source of
         Left e -> print e
         Right program -> do
             print $ runProgram program
+
+main :: IO ()
+main =  execParser opts >>= runWithOptions
+    where parser = Option <$> argument str (metavar "FILE")
+          opts = info parser mempty
